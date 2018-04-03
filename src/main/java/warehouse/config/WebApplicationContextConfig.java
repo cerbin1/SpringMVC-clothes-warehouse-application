e2b3397cc2047db89b909aae12b45df93f40c206
cerbin <gekarez@gmail.com>
2018-04-03 21:28:18 +0200

    Configure Jackson as Message converter

--- src/main/java/warehouse/config/WebApplicationContextConfig.java
+++ src/main/java/warehouse/config/WebApplicationContextConfig.java
@@ -2,9 +2,13 @@ package warehouse.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -13,5 +17,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converters.add(converter);
+        super.configureMessageConverters(converters);
     }
 }
