04fd530acbae6a801c2a7b746ffbcd3059a2c3ef
cerbin <gekarez@gmail.com>
2018-04-13 00:03:17 +0200

    Improve reason message

--- src/main/java/warehouse/exception/CategoryNotFoundException.java
+++ src/main/java/warehouse/exception/CategoryNotFoundException.java
@@ -4,6 +4,6 @@ package warehouse.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Category not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Items not found under this category")
 public class CategoryNotFoundException extends RuntimeException {
 }
