781b71848cbd9ca7b89c3ff61cc39b5495fe51a3
cerbin <gekarez@gmail.com>
2018-04-25 21:16:02 +0200

    Change annotation Controller to @RestController, remove redundant @ResponseBody from controller methods

--- src/main/java/warehouse/controller/ItemController.java
+++ src/main/java/warehouse/controller/ItemController.java
@@ -1,10 +1,9 @@
 package warehouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import util.StringUtils;
 import warehouse.domain.Item;
 import warehouse.exception.ArchivedNotFoundException;
@@ -12,7 +11,7 @@ import warehouse.service.ItemService;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ItemController {
     private final ItemService itemService;
 
@@ -22,43 +21,36 @@ public class ItemController {
     }
 
     @RequestMapping("items")
-    public @ResponseBody
-    List<Item> getAllItems() {
+    public List<Item> getAllItems() {
         return itemService.getAllItems();
     }
 
     @RequestMapping("items/{itemId:[0-9]+}")
-    public @ResponseBody
-    Item getItemById(@PathVariable String itemId) {
+    public Item getItemById(@PathVariable String itemId) {
         return itemService.getItemById(itemId);
     }
 
     @RequestMapping("items/{name:[a-zA-Z]+}")
-    @ResponseBody
     public Item getItemsByName(@PathVariable String name) {
         return itemService.getItemByName(name);
     }
 
     @RequestMapping("items/category/{category}")
-    @ResponseBody
     public List<Item> getItemByCategory(@PathVariable String category) {
         return itemService.getItemsByCategory(category);
     }
 
     @RequestMapping("items/color/{color}")
-    @ResponseBody
     public List<Item> getItemsByColor(@PathVariable String color) {
         return itemService.getItemsByColor(color);
     }
 
     @RequestMapping("items/size/{size}")
-    @ResponseBody
     public List<Item> getItemsBySize(@PathVariable String size) {
         return itemService.getItemsBySize(size);
     }
 
     @RequestMapping("items/archived/{archived}")
-    @ResponseBody
     public List<Item> getItemsByArchived(@PathVariable String archived) {
         if (StringUtils.isBoolean(archived)) {
             return itemService.getItemsByArchived(Boolean.parseBoolean(archived));
