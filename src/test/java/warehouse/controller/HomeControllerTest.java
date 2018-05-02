7bd342f6e44ca8772f71084b819076bb6a54ee88
cerbin <gekarez@gmail.com>
2018-05-02 17:42:24 +0200

    Add class HomeControllerTest

---
+++ src/test/java/warehouse/controller/HomeControllerTest.java
@@ -0,0 +1,52 @@
+package warehouse.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import warehouse.config.WebApplicationContextConfig;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebApplicationContextConfig.class)
+@WebAppConfiguration
+public class HomeControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldReturnWelcomeString() throws Exception {
+        // given
+        String welcomeMessage = "Warehouse application";
+
+        // when
+        MvcResult mvcResult = mockMvc
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        assertEquals(welcomeMessage,
+                mvcResult
+                        .getResponse()
+                        .getContentAsString());
+    }
+}
