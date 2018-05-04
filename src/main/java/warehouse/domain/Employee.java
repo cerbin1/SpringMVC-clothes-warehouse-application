f2072af31298e1a687dd7995b95052d7c21fc3af
cerbin <gekarez@gmail.com>
2018-05-04 20:48:37 +0200

    Add domain object Employee

---
+++ src/main/java/warehouse/domain/Employee.java
@@ -0,0 +1,31 @@
+package warehouse.domain;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+    private String surname;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
